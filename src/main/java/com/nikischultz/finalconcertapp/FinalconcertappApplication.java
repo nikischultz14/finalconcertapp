46767fda75567af90cad3d6022d1730d40399b0b
Nikita <nikita.schultz@astontech.com>
2018-06-28 11:39:45 -0500

    update 1

--- src/main/java/com/nikischultz/finalconcertapp/FinalconcertappApplication.java
+++ src/main/java/com/nikischultz/finalconcertapp/FinalconcertappApplication.java
@@ -3,10 +3,15 @@ package com.nikischultz.finalconcertapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class FinalconcertappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FinalconcertappApplication.class, args);
+
+
+
     }
 }
--- src/main/java/com/nikischultz/finalconcertapp/controllers/IndexController.java
+++ src/main/java/com/nikischultz/finalconcertapp/controllers/IndexController.java
@@ -1,15 +1,19 @@
 package com.nikischultz.finalconcertapp.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+//@RequestMapping({"/api"})
 public class IndexController {
 
-    @RequestMapping(value = {"/", "", "/index"})
+    @GetMapping(value = {"", "/", "/index"})
     public String index() {
         System.out.println("Hello There!");
-        return "/index";
+        return "/api";
 
     }
 
